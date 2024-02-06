db61839f40bb21e07d75afa933d3dec65edfc845
barisozdizdar <simonbateman90@gmail.com>
2024-02-06 22:49:33 +0100

    env file and api for login with google auth

--- game.py
+++ game.py
@@ -1,28 +1,29 @@
 import repository
 
 class Game():
-     def create_game_server():
-          
-          
-
+     
+     def create_game_server(): #for admins only
+     
           return 0
      
-     def play_game():
+     def play_game(): #for normal users
           #this is the init function for the games 
           #when the game starts the game screen with map and it's caches
           #will be shown  
-          
           #return the list of games current on mongo 
           return 0
      
      def leave_game():
           
-          
-          
           return 0 
      
-     def reset_game():
-          
-          
+     def reset_game(): #for admins only 
           
           return 0
+     
+     
+     def check_if_cache_image():
+          #first it is going to look at the location(lat/long)
+          #then it is going to look at the image
+          #this returns true if the cache image matches.
+          return 0 
--- geocaching_api.py
+++ geocaching_api.py
@@ -6,12 +6,37 @@ import repository
 from models import Game, User
 import game
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-import user_identification_service
+
+from starlette.requests import Request
+from starlette.middleware.sessions import SessionMiddleware
+from authlib.integrations.starlette_client import OAuth, OAuthError
+import json 
+import os   
+from authlib.integrations.starlette_client import OAuth, OAuthError
+from starlette.config import Config
+from starlette.responses import HTMLResponse, RedirectResponse
+from dotenv import load_dotenv
+import requests
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+config = Config('.env')
+oauth = OAuth(config)
 
+load_dotenv()  
+
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URIS")
+CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+VUE_REDIRECT_URL = "http://localhost:5173/map"
+oauth.register(
+    name='google',
+    server_metadata_url=CONF_URL,
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
+)
 
 origins = ["*"]
 app.add_middleware(
@@ -27,15 +52,34 @@ def create_game(request: Request, response: Response, game: Game = Body(...)):
     repository.create_game(request, response, game)
     return 0
 
-@app.post("/register")
-async def register_user(user: User):
-    return user_identification_service.register_user(user)
 
-@app.post("/login")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    return user_identification_service.user_login(form_data)
+@app.get("/login")
+async def login_google():
+    return {
+        "url": f"https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={GOOGLE_CLIENT_ID}&redirect_uri={GOOGLE_REDIRECT_URI}&scope=openid%20profile%20email&access_type=offline"
+    }
+
+@app.get("/auth")
+async def auth_google(code: str):
+    token_url = "https://accounts.google.com/o/oauth2/token"
+    data = {
+        "code": code,
+        "client_id": GOOGLE_CLIENT_ID,
+        "client_secret": GOOGLE_CLIENT_SECRET,
+        "redirect_uri": GOOGLE_REDIRECT_URI,
+        "grant_type": "authorization_code",
+    }
+    response = requests.post(token_url, data=data)
+    access_token = response.json().get("access_token")
+    user_info_response = requests.get("https://www.googleapis.com/oauth2/v1/userinfo", headers={"Authorization": f"Bearer {access_token}"})
+    user_info = user_info_response.json()
+    redirect_url = f"{VUE_REDIRECT_URL}?authenticated=true&user_info={user_info}"
+
+    return RedirectResponse(url=redirect_url)
 
 @app.post("/join_game")
 async def join_game():
     return game.play_game()
+
+
 
